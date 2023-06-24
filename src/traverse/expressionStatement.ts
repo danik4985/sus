297bf1ba34cc3416672cfc624f2b0a22db75b05b
danik4985 <danik.discord.alt@post.cz>
2023-06-24 15:28:25 +0200

    added support for directives

--- src/traverse/expressionStatement.ts
+++ src/traverse/expressionStatement.ts
@@ -4,7 +4,9 @@ import { callExpression } from './callExpression'
 import { rightExpression } from './rightExpression'
 
 export function expressionStatement(state: any, data = '') {
-	if (state.expression.type === 'AssignmentExpression') {
+	if (typeof state.directive === 'string') {
+		return JSON.stringify(state.directive) // support "use strict"
+	} else if (state.expression.type === 'AssignmentExpression') {
 		return assignmentExpression(state.expression, data)
 	} else if (state.expression.type === 'CallExpression') {
 		return callExpression(state.expression)
