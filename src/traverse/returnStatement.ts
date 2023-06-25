3b51d1ef58d3ad6816c916a6cfac6e9ef338bc70
danik4985 <danik.discord.alt@post.cz>
2023-06-25 15:16:42 +0200

    improved returnStatement

--- src/traverse/returnStatement.ts
+++ src/traverse/returnStatement.ts
@@ -2,10 +2,13 @@ import { comment } from '../obfuscate/comment'
 import { rightExpression } from './rightExpression'
 
 export function returnStatement(expr: any, data = '') {
-	data += 'return '
+	data += 'return('
 	data += comment(2)
 
 	if (expr.argument) data += rightExpression(expr.argument)
+	else data += 'void(0)'
+
+	data += ')'
 
 	return data
 }
